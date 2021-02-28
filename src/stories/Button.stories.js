af50c658c99ddddd876cd3debfc61149f6c1d5d4
Lucio Cabrera <Lucio.Cabrera@Just-BI.nl>
2021-02-28 16:21:12 +0100

    Add Button story

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '../components/Button';
+
+export default {
+  title: 'Components/Core/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    fontColor: { control: 'color' }
+  }
+};
+
+const Template = args => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Button'
+};
