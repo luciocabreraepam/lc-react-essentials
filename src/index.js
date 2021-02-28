dca07e160ff3d8dfb0dd72c2609e0494b5f8830c
Lucio Cabrera <Lucio.Cabrera@Just-BI.nl>
2021-02-28 16:29:59 +0100

    Add index to export components

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export * from './components/Button';
+export * from './components/Parallax';
+export * from './components/ParallaxBlock';
+export * from './components/ContentHolder/ContentBlock';
+export * from './components/ContentHolder/DescriptionBlock';
+export * from './components/Header/HeaderImpact';
